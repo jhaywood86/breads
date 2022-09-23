79f9aca2ea2b826dd3cf4d017dc1c6e1d26bff54
Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
2022-09-22 18:55:08 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+// models are what data is supposed to look like or has to look like.
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
 })
+
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+  res.render('Show', {
+    bread: Bread[req.params.arrayIndex]
   })
-  
+})
+
 
 module.exports = breads
